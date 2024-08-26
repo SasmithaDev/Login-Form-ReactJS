e5d3d4e8d9d6609db917d1b97e18b86d1809ab75
Sasmitha Weerakkody 🇪🇸 <151928231+SasmithaDev@users.noreply.github.com>
2024-08-27 00:09:59 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import FlipForm from './components/FlipForm';
+
+function App() {
+  return (
+    <div className="App">
+      <FlipForm />
+    </div>
+  );
+}
+
+export default App;
